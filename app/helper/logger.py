fee99c07fc0303b7a964a2cc44b7064bd1a1fdc6
Quoc Tuan <popopotuan0188@gmail.com>
2025-10-22 15:49:14 +0700

    update logger

--- app/helper/logger.py
+++ app/helper/logger.py
@@ -20,9 +20,12 @@ def get_logger(name:str,base_log_dir: str = 'log_file', log_to_file: bool = True
     logger.addHandler(console_handler)
     # File Handler
     if log_to_file:
-        os.makedirs(base_log_dir,exist_ok= True)
-        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        log_path = os.path.join(base_log_dir, f"{name}_{timestamp}.log")
+        # Tao thu muc theo module
+        module_dir = os.path.join(base_log_dir,name)
+        os.makedirs(module_dir,exist_ok= True)
+        
+        
+        log_path  = os.path.join(module_dir,f"{name}.log")
         
         file_handler = logging.FileHandler(log_path,encoding='utf-8')
         file_handler.setFormatter(formatter)
